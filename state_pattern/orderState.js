8427052485812ba17c7b809eaf25920c21906c76
NewtonKamau <kamaunewton78@gmail.com>
2023-06-13 10:23:53 +0300

    orderPendingState calling process()

--- state_pattern/orderState.js
+++ state_pattern/orderState.js
@@ -6,7 +6,12 @@ class OrderState{
     }
 }
 
-
-
+class OrderPendingState extends OrderState{
+    process () {
+        console.log("Order is pending. Processing the order.....")
+        //logic for processing the pending order
+        this.order.setState(new OrderShippedState(this.order));
+    }
+}
 
 
