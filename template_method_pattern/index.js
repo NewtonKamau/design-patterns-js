cc5f931f90f53b05d78cd21b8afff9ebcf9d9f67
NewtonKamau <kamaunewton78@gmail.com>
2023-06-14 16:25:24 +0300

    implementation of the template pattern

---
+++ template_method_pattern/index.js
@@ -0,0 +1,12 @@
+const { WoodenHouseBuilder, BrickHouseBuilder, WoodenHouseBuilder } = require('./template');
+
+
+const woodenHouseBuilder = new WoodenHouseBuilder();
+
+woodenHouseBuilder.build();
+
+console.log('...........................');
+
+const brickHouseBuilder = new BrickHouseBuilder()
+
+brickHouseBuilder.build()
